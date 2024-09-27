05e9618f60ce57de862d358c694f4232b8377cb9
Charles Ezeobi <charlesezeobi3@gmail.com>
2024-09-27 14:57:20 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+let hamburger = document.querySelector('.hamburger');
+let mobileMenu = document.querySelector('.mobile-menu');
+        
+hamburger.addEventListener('click', function(){
+mobileMenu.classList.toggle('active')});
